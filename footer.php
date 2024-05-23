a8c876fed300b69d3c0434e6faa556dba07ceeb7
Tycho-VH <150657502+Tycho-VH@users.noreply.github.com>
2024-05-23 13:36:50 +0200

    commiten in mijn eigen branch

---
+++ footer.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/footer.css">
+    <title>Document</title>
+</head>
+<body>
+    
+  <body>
+    <html>
+  
+        <section id="footer">
+    <div class="footerbuttons">
+    <h3>Vluchten</h3>
+    <h3>Home</h3>
+    <h3>Over ons</h3>
+    <h3>Contact</h3>
+    </div>
+    <div class="nieuwsbrief">
+       <form action="">
+        <data value="text"></data>
+       </form>
+    </div>
+        </section>
+        </html>
+        </body>
+</body>
+</html>
