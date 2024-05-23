1e4a4ca2999b659e76bbee6974666ee4a6441bc5
Wschiks <1206720@student.roc-nijmegen.nl>
2024-05-23 13:19:23 +0200

    committen voor het wisselen naar andere branch

--- header.php
+++ header.php
@@ -1,10 +1,53 @@
+<!DOCTYPE html>
+<link rel="stylesheet" href="css/headerstyle.css">
 
+<!-- font -->
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Poetsen+One&display=swap" rel="stylesheet">
 
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link
+    href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+    rel="stylesheet">
+
+<html>
+
+<head>
+
+</head>
 
 <body>
-<link rel="stylesheet" href="css/headerstyle.css">
-<section id="header"></section>
+
+
+    <section id="header" class="width100 blauw flex midden">
+
+            <div class="flex"><img src="img/logo.png" alt="">
+
+                <h2 class="white font">Flieg.com</h2>
+            </div>
+
+
+
+
+            <div class="flex headerbuttons midden">
+                <h3>Home</h3>
+                <h3>Fliegen</h3>
+                <h3>Over ons</h3>
+                <h3>Contact</h3>
+            </div>
+
+            <div class="geel Login flex">
+                <h3>Login</h3>
+            </div>
+
+
+    </section>
+
 
 
 
 </body>
+
+</html>
