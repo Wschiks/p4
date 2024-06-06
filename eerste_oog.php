565668368a688698814fc512d7be0b019f4da033
Tycho-VH <150657502+Tycho-VH@users.noreply.github.com>
2024-06-06 13:51:52 +0200

    fotos

--- eerste_oog.php
+++ eerste_oog.php
@@ -1,41 +1,44 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-<link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Poetsen+One&display=swap" rel="stylesheet">
- 
-<link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link
-    href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900&display=swap"
-    rel="stylesheet">
- 
- <meta charset="UTF-8">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poetsen+One&display=swap" rel="stylesheet">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900&display=swap"
+        rel="stylesheet">
+
+    <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/footer.css">
     <title>Document</title>
 
 </head>
+
 <body>
     <section id="eersteoog">
-       <div class="eersteoog1">
-        <div class="eersteoog_text">
-            <div class="titleplek">
-<h2 class="font fontgroot">Flieg.com </h2>
-</div>
-<h3 class="kleinetext">Reis op jouw eigen unieke manier. Koester
-je dromen en ontdek de meest
-betoverende locaties die jouw hart sneller
-laten kloppen. flieg met ons!</h3>
+        <div class="eersteoog1">
+            <div class="eersteoog_text">
+                <div class="titleplek">
+                    <h2 class="font fontgroot">Flieg.com </h2>
+                </div>
+                
+                    <h3 class="kleinetext"> Reis op jouw eigen unieke manier. Koester
+                        je dromen en ontdek de meest
+                        betoverende locaties die jouw hart sneller
+                        laten kloppen.flieg met ons!</h3>
+               
+            </div>
 
-</div>
+            <img src="img/eerste_oog.png" alt="Overlappende Afbeelding" class="overlap-image">
 
-<img src="img/eerste_oog.png" alt="Overlappende Afbeelding" class="overlap-image">
+        </div>
+        <div class="eersteoog2">
 
-       </div>
-<div class="eersteoog2">
-
-</div>
+        </div>
     </section>
 </body>
